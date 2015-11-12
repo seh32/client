2b3df46e3007f7cb04da62b34672de4100698aa8
Arie van Luttikhuizen <aluttik@gmail.com>
2015-11-12 18:49:53 -0500

    Refactored the card template

---
+++ client/views/cards/cards.js
@@ -0,0 +1,31 @@
+// binds to the cards template when its created
+Template.cards.onCreated(function(){
+  this.subscribe("bookshelf");
+});
+
+// helper functions bound to the cards template
+Template.cards.helpers({
+    bookshelf: function() { 
+        return Bookshelf.find(); 
+    }
+});
+
+// binds to the card template when its created
+Template.card.onCreated(function() {
+    this.messageSent = new ReactiveVar(false);
+});
+
+// event handler bound to the card template
+Template.card.events({
+    "click .send-message": function(event, template) {
+        template.messageSent.set(true);
+        Materialize.toast("Message Sent", 4000, "rounded");
+    }
+})
+
+// helper functions bound to the card template
+Template.card.helpers({
+    messageSent: function() {
+        return Template.instance().messageSent.get();
+    }
+});
--- client/views/cards/helpers.js
+++ client/views/cards/helpers.js
@@ -3,3 +3,20 @@ Template.cards.helpers({
         return Bookshelf.find(); 
     }
 });
+
+Template.card.onCreated(function() {
+    this.messageSent = new ReactiveVar(false);
+});
+
+Template.card.helpers({
+    messageSent: function() {
+        return Template.instance().messageSent.get();
+    }
+});
+
+Template.card.events({
+    "click .send-message": function(event, template) {
+        template.messageSent.set(true);
+        Materialize.toast("Message sent", 2000, "rounded");
+    }
+});
