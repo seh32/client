dd2501d87316216b630f8719621f7a7e6a3ebfba
Arie van Luttikhuizen <aluttik@gmail.com>
2015-11-27 21:13:00 -0500

    Replaced Bookshelf with books and classes

--- server/publications.js
+++ server/publications.js
@@ -4,35 +4,18 @@
  * To do: filter search results based on search parameters
  *        sort results by price, name, ect.
  */
-Meteor.publish('bookshelf',function(){
-    return Bookshelf.find();
+Meteor.publish('books', function() {
+    return Books.find();
 });
-
-if (Bookshelf.find().count() === 0) {
-  Bookshelf.insert({
-    title: "Fundamentals of Chemistry",
-    author: "Ralph A. Burns",
-    thumbnail: "http://www.thomasriggs.net/blog/wp-content/uploads/2009/08/TextbookCover.jpg"
-  });
-
-  Bookshelf.insert({
-    title: "Human Biology",
-    author: "Michael D. Johnson",
-    thumbnail: "http://homepage.smc.edu/azuma_kay/bio2/images/Johnson4.jpg"
-  });
-
-  Bookshelf.insert({
-    title: "Essential English",
-    author: "E. Suresh Kumar, P. Sreehari, J. Savithri",
-    thumbnail: "http://www.languageinindia.com/april2011/essentialenglishfrontcover.jpg"
-  });
-}
+Meteor.publish('classes', function() {
+    return Classes.find();
+});
 
 Meteor.methods({
   newBook: function (book) {
-    Bookshelf.insert(book);
+    Books.insert(book);
   },
   removeBook: function (id) {
-    Bookshelf.remove(id);
+    Books.remove(id);
   }
-})
+});
